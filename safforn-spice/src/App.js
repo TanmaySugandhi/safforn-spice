af41ef43b7a13c1b7768816ce813108a7c1f968e
Tanmay Sugandhi <tanmayedu2@gmail.com>
2025-08-10 17:13:34 +0530

    Initial commit
    Converting to React Web-Major

---
+++ safforn-spice/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Styles/App.css';
+import NavBar from './Componenets/NavBar.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ safforn-spice/src/Componenets/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../Styles/NavBar.css';
+
+function NavBar() {
+  return (
+    <>
+        <header>
+        <div class="logo">
+            <img src="public/Pictures/logo.avif" alt="Restaurant Logo"></img>
+            <h1>Saffron Spice</h1>
+        </div>
+        <nav>
+            <a href="#about">About Us</a>
+            <a href="#menu">Menu</a>
+            <a href="#chefs">Chefs</a>
+            <a href="#pricing">Pricing</a>
+            <a href="#contact">Contact</a>
+        </nav>
+    </header>
+    </>
+  );
+}
+
+export default NavBar;
